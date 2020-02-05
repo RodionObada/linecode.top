231469c6c5a5a87ade8fd2cebb69a0bb2632ef9a
RodionObada <r.obada@mail.ru>
2020-02-05 15:23:13 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,5 +19,61 @@ $(function(){
 		$(this).toggleClass('header__burger-menu--close');
 
 		$('.menu').toggle(200);
+  });
+});
+
+$(function () {
+
+	function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+ 	function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+	$('.popup-button').click(function(){
+		$('.popup-container').fadeIn(400, disableScroll);
+		$('.popup').animate({
+			width: '370px',
+			height: '430px'
+		}, 400);
 	});
+
+	$('.popup-container').click(function(event){
+		if(event.target == this){
+			$(this).fadeOut(400, enableScroll);
+			$('.popup').animate({
+			width: 0,
+			height: 0
+		}, 400);
+		}
+	})
 });
