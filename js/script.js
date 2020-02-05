479b76ae52e205d1b83ab4a174ce3e2e3acff2ae
RodionObada <r.obada@mail.ru>
2020-02-05 15:50:48 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,40 +25,15 @@ $(function(){
 $(function () {
 
 	function disableScroll() {
-  if (window.addEventListener) // older FF
-      window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove  = preventDefault; // mobile
-  document.onkeydown  = preventDefaultForScrollKeys;
+    $('html, body').on('mousewheel', function(){
+      return false;
+    });
 }
 
  	function enableScroll() {
-    if (window.removeEventListener)
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null;
-    window.onwheel = null;
-    window.ontouchmove = null;
-    document.onkeydown = null;
+    $('html, body').off('mousewheel');
 }
 
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-      e.preventDefault();
-  e.returnValue = false;
-}
-
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
 	$('.popup-button').click(function(){
 		$('.popup-container').fadeIn(400, disableScroll);
 		$('.popup').animate({
@@ -68,12 +43,13 @@ function preventDefaultForScrollKeys(e) {
 	});
 
 	$('.popup-container').click(function(event){
-		if(event.target == this){
+		if(event.target == this) {
 			$(this).fadeOut(400, enableScroll);
-			$('.popup').animate({
+			$('.popup').animate(
+        {
 			width: 0,
 			height: 0
-		}, 400);
+		    }, 400);
 		}
-	})
+	});
 });
