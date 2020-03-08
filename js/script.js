02d37865572d6294dbcee5a3b0724cce94457b66
RodionObada <r.obada@mail.ru>
2020-03-08 13:33:12 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,53 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $('.fly-elements').addClass('fly-elements--active');
 
-});
-
-// Бурег меню
-$(function(){
-	$('.header__burger-menu').click(function(e){
+// Выпадающее меню
+  $('.header__burger-menu').click(function(e) {
 		e.preventDefault();
 		$(this).toggleClass('header__burger-menu--close');
 
 		$('.menu').toggle(200);
   });
-});
 
 // Поп ап
-$(function () {
-// 	function disableScroll() {
-//     $('html, body').on('mousewheel', function(){
-//       return false;
-//     });
-// }
-
-//  	function enableScroll() {
-//     $('html, body').off('mousewheel');
-// }
-
-	$('.popup__button').click(function(){
+  $('.popup__button').click(function() {
     $('html').addClass('scroll-off');
 		$('.popup').fadeIn(400);
-		$('.popup__form').animate({
-			width: '370px',
-			height: '430px'
-		}, 400);
+		$('.popup__form').fadeIn(400);
 	});
 
-	$('.popup').click(function(event){
+	$('.popup').click(function(event) {
 		if(event.target == this) {
       $('html').removeClass('scroll-off');
 			$(this).fadeOut(400);
-			$('.popup__form').animate(
-        {
-			width: 0,
-			height: 0
-		    }, 400);
+			$('.popup__form').fadeOut(400);
 		}
 	});
 });
-
 
 // Слайдер
 new Swiper('.slider', {
